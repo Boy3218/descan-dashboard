2288fe7388fbbe909c2b1588477cc5844916fb7b
Boy3218 <bps.3218.medsos@gmail.com>
2026-04-30 08:59:33 +0700

    Tangkap semua error fatal secara paksa

--- api/index.php
+++ api/index.php
@@ -5,8 +5,14 @@
  */
 
 ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+echo "PHP Version: " . phpversion() . "<br><br>";
+
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    die("ERROR FATAL: Folder 'vendor' tidak ditemukan! Artinya Vercel gagal melakukan proses 'composer install' saat deploy. Cek versi PHP Vercel vs Laravel.");
+}
 
 putenv('VIEW_COMPILED_PATH=/tmp');
 putenv('APP_CONFIG_CACHE=/tmp/config.php');
@@ -15,4 +21,9 @@ putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
 putenv('APP_ROUTES_CACHE=/tmp/routes.php');
 putenv('APP_SERVICES_CACHE=/tmp/services.php');
 
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1>Error dari dalam Laravel:</h1>";
+    echo "<pre>" . (string) $e . "</pre>";
+}
