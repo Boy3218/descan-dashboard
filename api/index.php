fed5a0e42c49de731c2f3f9dc8c6e863bc77c151
Boy3218 <bps.3218.medsos@gmail.com>
2026-04-30 08:18:41 +0700

    Fix Vercel read-only filesystem 500 error

--- api/index.php
+++ api/index.php
@@ -3,4 +3,12 @@
 /**
  * Forward Vercel requests to the normal Laravel index.php
  */
+
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+
 require __DIR__ . '/../public/index.php';
