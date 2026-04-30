7e0251643c9cc37339ff58f9d9a84e20e5eb276f
Boy3218 <bps.3218.medsos@gmail.com>
2026-04-30 08:27:28 +0700

    Fix storage path for Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,3 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+if (isset($_ENV['VERCEL_URL']) || getenv('VERCEL_URL') || getenv('VERCEL')) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+return $app;
