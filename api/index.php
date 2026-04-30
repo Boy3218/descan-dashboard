f197e7f91cafbb590bd3953b6c9c40ae09eaaf10
Boy3218 <bps.3218.medsos@gmail.com>
2026-04-30 09:26:23 +0700

    Deteksi Vercel Environment Variables

--- api/index.php
+++ api/index.php
@@ -21,6 +21,14 @@ putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
 putenv('APP_ROUTES_CACHE=/tmp/routes.php');
 putenv('APP_SERVICES_CACHE=/tmp/services.php');
 
+$appKey = getenv('APP_KEY') ?: $_ENV['APP_KEY'] ?? $_SERVER['APP_KEY'] ?? null;
+if (!$appKey) {
+    die("<h1>ERROR: Vercel belum membaca APP_KEY!</h1><p>Variabel APP_KEY benar-benar kosong di sistem Vercel. Pastikan Anda mencentang opsi 'Production' saat menambahkan variabel di Vercel.</p>");
+} else {
+    // Memastikan APP_KEY masuk ke putenv agar bisa dibaca oleh fungsi env() Laravel
+    putenv('APP_KEY=' . $appKey);
+}
+
 try {
     require __DIR__ . '/../public/index.php';
 } catch (\Throwable $e) {
